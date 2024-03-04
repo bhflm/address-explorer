078f3ed2cd52571841064087cd477a04bf57b8af
germi <germanbonin@gmail.com>
2024-03-03 21:44:35 -0300

    fix: broken src image due to case sensitive ref

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
     <header className="">
       <nav className="mx-auto flex items-center justify-between p-6 sm:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <Image src="icon.svg" alt="WalletConnect dark logo" width={32} height={40} />
+          <Image src="wc.svg" alt="WalletConnect dark logo" width={32} height={40} />
           <p className="text-white mx-4 text-base sm:text-xl ">WalletConnect</p>
         </div>
       </nav>
